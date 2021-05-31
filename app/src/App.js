cd99244e02fa2630e4f6ffca5a1e50194af0e609
Markus Vleno <markus.vleno.personal@gmail.com>
2021-05-31 19:25:23 +0530

    31/05/21(night)

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,9 @@
+import React from "react";
 
-function App() {
-    return <div className="App">Hello world</div>;
+class App extends React.Component {
+    render() {
+        return <div>Hello world</div>;
+    }
 }
 
 export default App;
