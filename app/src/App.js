f12833b37b329a7c7f6c8294f38affdcfb501cc5
Markus Vleno <markus.vleno.personal@gmail.com>
2021-06-02 20:45:21 +0530

    02/06/21

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
+import "./css/base.css";
+import "./css/template.css";
 
 class App extends React.Component {
     render() {
-        return <div>Hello world</div>;
+        return (
+            <div className="base">
+                <div className="template m-menu-area">
+                    <div className="logo"></div>
+                    <div className="options"></div>
+                    <div className="search"></div>
+                    <div className="users"></div>
+                    <div className="converstion"></div>
+                </div>
+            </div>
+        );
     }
 }
 
