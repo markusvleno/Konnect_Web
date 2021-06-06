f50a6d834b2cfe29622cc4b85ba7ee089794a057
Markus Vleno <markus.vleno.personal@gmail.com>
2021-06-06 14:22:50 +0530

    06/06/21

--- app/src/components/Option.js
+++ app/src/components/Option.js
@@ -1,29 +1,17 @@
 import "../css/option.css";
 import React from "react";
 
-import logo_conv from "../images/conversation.svg";
-import logo_add from "../images/add_friend.svg";
-import logo_sett from "../images/settings.svg";
-import logo_out from "../images/logout.svg";
-
+import { conversation, addFriend, settings, logout } from "../images/svgs";
 class Option extends React.Component {
     render() {
         return (
             <div className="option">
                 <div className="user-detail"></div>
                 <div className="controls">
-                    <div className="tab-conversation">
-                        <img src={logo_conv} alt="logo conversation" />
-                    </div>
-                    <div className="tab-add">
-                        <img src={logo_add} alt="logo add friends" />
-                    </div>
-                    <div className="tab-settings">
-                        <img src={logo_sett} alt="logo settings" />
-                    </div>
-                    <div className="tab-logout">
-                        <img src={logo_out} alt="logo logout" />
-                    </div>
+                    <div className="control-svg tab-conversation">{conversation()}</div>
+                    <div className="control-svg tab-add">{addFriend()}</div>
+                    <div className="control-svg tab-settings">{settings()}</div>
+                    <div className="control-svg tab-logout">{logout()}</div>
                 </div>
                 <div className="me-photo"></div>
             </div>
