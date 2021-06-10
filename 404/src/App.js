8767271e7a1fe358708286a082a8c238decea4c5
Markus Vleno <markus.vleno.personal@gmail.com>
2021-06-10 21:14:07 +0530

    10/06/21(night)

---
+++ 404/src/App.js
@@ -0,0 +1,33 @@
+import "./style.css";
+import React from "react";
+import {
+    _404,
+    email,
+    gear1,
+    gear2,
+    gear3,
+    gear4,
+    gear5,
+    gear6,
+    gear7,
+    gear8,
+    particle1,
+    particle2,
+    particle3,
+    robot,
+} from "./images/Svgs";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="base">
+                <div className="robot-base">
+                    <div className="robot">{robot()}</div>
+                </div>
+                <span className="oops"></span>
+            </div>
+        );
+    }
+}
+
+export default App;
