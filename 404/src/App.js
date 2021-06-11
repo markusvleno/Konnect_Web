cb7e07d00e45150a4dfdae0df391fc845aa011dd
Markus Vleno <markus.vleno.personal@gmail.com>
2021-06-11 19:58:18 +0530

    11/06/21(night)

--- 404/src/App.js
+++ 404/src/App.js
@@ -1,30 +1,17 @@
 import "./style.css";
 import React from "react";
-import {
-    _404,
-    email,
-    gear1,
-    gear2,
-    gear3,
-    gear4,
-    gear5,
-    gear6,
-    gear7,
-    gear8,
-    particle1,
-    particle2,
-    particle3,
-    robot,
-} from "./images/Svgs";
+import { robotIlustration, gear1, gear2, gear3, gear4 } from "./images/Svgs";
 
 class App extends React.Component {
     render() {
         return (
             <div className="base">
                 <div className="robot-base">
-                    <div className="robot">{robot()}</div>
-                    <div className="_404">{_404()}</div>
-                    <div className="gear1">{gear1()}</div>
+                    {robotIlustration()}
+                    <div>{gear1()}</div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+                    <div>{gear2()}</div>
+                    <div>{gear3()}</div>
+                    <div>{gear4()}</div>                                                                                                                        
                 </div>
                 <span className="oops"></span>
             </div>
