914d0b75aacf0b8d3dcbeb4d0b328454966d9459
Markus Vleno <markus.vleno.personal@gmail.com>
2021-06-12 19:43:34 +0530

    12/06/21(night)

--- app/src/components/Conversation.js
+++ app/src/components/Conversation.js
@@ -1,11 +1,13 @@
 import "../css/conversation.css";
 import React from "react";
+import UserDetail from "./UserDetail";
 
 class Conversation extends React.Component {
     render() {
         return (
             <React.Fragment>
                 <div className="btn-back"></div>
+                <UserDetail />
                 <div className="conversation"></div>
             </React.Fragment>
         );
