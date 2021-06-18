2770a3ab66d3de7fa8559d4470960046c8242d76
Markus Vleno <markus.vleno.personal@gmail.com>
2021-06-18 11:31:25 +0530

    18/06/21

--- home/src/Home.jsx
+++ home/src/Home.jsx
@@ -1,15 +1,9 @@
 import "./css/base.css";
 import React from "react";
 
-import { Loading } from "./images/Svg";
-
 class Home extends React.Component {
     render() {
-        return (
-            <div className="base">
-                <Loading />
-            </div>
-        );
+        return <div className="base"></div>;
     }
 }
 
