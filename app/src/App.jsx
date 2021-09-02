a68b8e7bec142c31ad91025843f185b003372c66
Markus Vleno <markus.vleno.personal@gmail.com>
2021-09-02 13:54:51 +0530

    02/09/21

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -7,31 +7,29 @@ import Search from "./components/Search";
 import Users from "./components/Users";
 import Conversation from "./components/Conversation";
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className="base">
-                <div className="template m-menu-area">
-                    <div className="btn-back"></div>
-                    <div className="template-logo">
-                        <Logo />
-                    </div>
-                    <div className="template-options">
-                        <Option />
-                    </div>
-                    <div className="template-search">
-                        <Search />
-                    </div>
-                    <div className="template-users">
-                        <Users />
-                    </div>
-                    <div className="template-converstion">
-                        <Conversation />
-                    </div>
+const App = (props) => {
+    return (
+        <div className="base">
+            <div className="template m-menu-area">
+                <div className="btn-back"></div>
+                <div className="template-logo">
+                    <Logo />
+                </div>
+                <div className="template-options">
+                    <Option />
+                </div>
+                <div className="template-search">
+                    <Search />
+                </div>
+                <div className="template-users">
+                    <Users />
+                </div>
+                <div className="template-converstion">
+                    <Conversation />
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default App;
--- app/src/redux/slice/user.js
+++ app/src/redux/slice/user.js
@@ -15,17 +15,26 @@ export const user = createSlice({
             state = action.payload;
         },
         updateName: (state, action) => {
-            state.name = action.payload;
+            state.name = action.payload.name;
         },
         updateProfilePicture: (state, action) => {
-            state.profilePicture = action.payload;
+            state.profilePicture = action.payload.profilePicture;
         },
-        newChat: (state, action) => {
+        newConversation: (state, action) => {
             state.conversation = new Array(...state.conversation, action.payload);
+        },
+        deleteConversation: (state, action) => {
+            const conversation = action.payload;
+            const newState = state.conversation.forEach((conv) => {
+                if (conv.username !== conversation.username);
+                return conv;
+            });
+
+            state.conversation = new Array(newState);
         },
     },
 });
 
-export const {} = user.actions;
+export const { init, updateName, updateProfilePicture, newConversation, deleteConversation } = user.actions;
 
 export default user.reducer;
